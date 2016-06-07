64f634aba7bd3c832847ecc8c9a51600a22f6bd7
Eldar Aliyev <eldar.aliyev8@gmail.com>
2016-06-08 00:18:19 +0600

    Fix gitignore, add pasic webpack config

---
+++ webpack.config.babel.js
@@ -0,0 +1,45 @@
+// import webpack import 'webpack';
+// import path import 'path';
+
+import precss from 'precss';
+import stylelint from 'stylelint';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+
+export default {
+  entry: {
+    app: './src/app.js',
+    vendor: ['react', 'react-dom'],
+  },
+
+  output: {
+    path: 'build',
+    publicPath: '/',
+    filename: 'scripts/[name].js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?modules&localIdentName=[name]__[local]__[hash:base64:5]!postcss',
+      },
+    ],
+  },
+
+  postcss() {
+    return [
+      precss,
+      stylelint,
+      autoprefixer({
+        browsers: ['> 1%', 'last 15 versions'],
+      }),
+      cssnano,
+    ];
+  },
+};
